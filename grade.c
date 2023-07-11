28d4a5d520f8535e79bc1b27d796f57f5e38a473
Fazman S <fazmansam@gmail.com>
2023-07-11 23:15:45 +0530

    Update grade.c

--- grade.c
+++ grade.c
@@ -1,26 +1,29 @@
-#include<stdio.h>
-int main()
-{
-float per;
-char grade;
-scanf("%f",&per);
-if(per>=90)
-grade= 'A';
-else if(per>=80 && per<90)
-grade ='B';
-else if(per>=70 && per<80)
-grade ='C';
-else if(per>=60 && per<70)
-grade='D';
-else grade='E';
-switch(grade)
-{
-case 'A': printf("\nEXCELLENT"); break;
-case 'B':printf("\nVery Good"); break;
-case 'C' : printf("\nGood"); break;
-case 'D': printf("\nAbove Average"); break;
-case 'E': printf("\n Satisfactory"); break;
-}
-printf("\t The percentage = %f and grade is %c ",per,grade);
-return 0;
+#include <stdio.h>
+
+int main() {
+  float per;
+  char grade;
+  scanf("%f",&per);
+  if(per >= 90)
+    grade ='A';
+  else if(per>=80 && per <90)
+    grade='B';
+  else if(per>=70 && per <80)
+    grade='C';
+  else if(per>=60 && per <70)
+    grade='D';
+  else grade='E';
+
+  switch(grade){
+    case 'A': printf("\n Execellent");break;
+    case 'B': printf("\n Very Good");break;
+    case 'C': printf("\n Good");break;
+    case 'D': printf("\n Above Average");break;
+    case 'E': printf("\n Satisfactory");break;
+  }
+
+  printf("\n The Percentage =%f and grade is %c :",per,grade);
+
+  
+  return 0;
 }
